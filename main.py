c6849ca7272d1ad037aba6c0b1b5cab663944742
Татьяна <tpaporkova@gmail.com>
2022-05-24 12:17:55 +0300

    названия диаграмм

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ this program charts the 10 cheapest and 10 most popular courses
 '''
 
 # pylint C:\Users\Татьяна\python\зачёт\udemy.py
-# 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
 
 import pandas as pd
 import numpy as np
@@ -19,11 +18,13 @@ while t['price'][i] == 0:
 paid = t[i:].reset_index(drop=True)    # only paid courses
 cheap = paid[:10]                      # 10 cheapest courses
 x = np.arange(1, 11)
+plt.title("10 самых дешёвых курсов")
 plt.bar(x, cheap['price'])
 plt.show()
 
 sort_popular = paid.sort_values(by='num_subscribers', ascending=False).reset_index(drop=True)
 popular = sort_popular[:10]            # 10 most popular courses
 x = np.arange(1, 11)
+plt.title("10 самых популярных курсов")
 plt.bar(x, popular['num_subscribers'])
 plt.show()
